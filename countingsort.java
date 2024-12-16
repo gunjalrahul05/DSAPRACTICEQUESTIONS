081d62f1efeff8faf0192051b09b42d48599e93b
gunjalrahul05 <gunjalrahul4142@gmail.com>
2024-12-16 19:03:34 +0530

    countingsort

--- countingsort.java
+++ countingsort.java
@@ -1,38 +1,28 @@
+public class countingsort {
+    public static void main(String[] args) {
+        int arr[] = { 1, 4, 1, 3, 2, 4, 3, 7 };
+        int n = arr.length;
 
-public class countingsort{
-    public static void countingsort1(int[] arr){
-        int n=arr.length;
-
-        int largest = Integer.MIN_VALUE;
-
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            largest = Math.max(largest,arr[i]);
+            max = Math.max(max, arr[i]);
         }
+        int count[] = new int[max+1];
 
-        int count[] = new int[largest+1];
-
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             count[arr[i]]++;
         }
 
-        int j=0;
-        for(int i=0;i<count.length;i++){
-            while(count[i]>0){
-                arr[j]=i;
-                j++;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            while (count[i] != 0) {
+                arr[cnt] = i;
+                cnt++;
                 count[i]--;
             }
         }
-    }
-    public static void display(int[] arr){
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+"");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-    }
-    public static void main(String[] args){
-        int arr[] = {1,4,1,3,2,4,3,7};
-
-        countingsort.countingsort1(arr);
-        countingsort.display(arr);
     }
 }
